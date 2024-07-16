1acacece2be5fe7c7be0e85ce4b392d2827966a7
muchubatactics <byooxmckeith@gmail.com>
2024-07-16 07:33:11 +0300

    Style: Shop page

--- src/components/shop.jsx
+++ src/components/shop.jsx
@@ -1,10 +1,18 @@
 import Shopheader from "./shopHeader.jsx";
+import { Link, Outlet } from "react-router-dom";
+
+import shopstyles from '../styles/shop.module.css';
 
 export default function Shop() {
   return (
     <>
       <Shopheader />
-      <div>this is the shop</div>
+      <main>
+        <nav className={shopstyles.sidebar}>
+          <Link></Link>
+        </nav>
+        <Outlet className={shopstyles.maingoa}></Outlet>
+      </main>
     </>
   );
 }
