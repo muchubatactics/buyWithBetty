ec1685f464769742cff9c553a9ab5ac87409117d
muchubatactics <byooxmckeith@gmail.com>
2024-07-18 13:02:00 +0300

    Add: fetching data from rawg

--- src/components/games.jsx
+++ src/components/games.jsx
@@ -1,9 +1,54 @@
 import { useParams } from "react-router-dom";
 
 import styles from '../styles/games.module.css';
+import { useEffect, useState } from "react";
 
 export default function Games() {
   let { gamesId } = useParams();
+  const [loading, setLoading] = useState(true);
+  const [games, setGames] = useState([]);
+
+  // const ky = '0d8f5888225b4c37b78af65363f639cd';
+  const ky = '';
+
+  useEffect(() => {
+    let res = [];
+
+    fetch(`https://api.rawg.io/api/games?key=${ky}`, {
+      method: "GET",
+      mode: "cors",
+      headers: {
+        'Content-type': 'application/json',
+      }
+    }).then((Response) => {
+      return Response.json();
+    }).then((Response) => {
+      for (let i = 0; i < Response.results.length; i++) {
+        let obj = {
+          id: Response.results[i].id,
+          name: Response.results[i].name,
+          background_image: Response.results[i].background_image,
+          release_date: Response.results[i].released,
+          dominant_color: Response.results[i].dominant_color,
+          genres: null,
+          platforms: null,
+          screenshots: [...Response.results[i].short_screenshots],
+        }
+
+        res.push(obj);
+      }
+      
+    }).catch((err) => {
+      console.error(err);
+    }).finally(() => {
+      setGames(res);
+      setLoading(false);
+    });
+
+    return () => {
+
+    }
+  }, []);
 
   return (
     <div className={styles.games}>
@@ -15,6 +60,21 @@ export default function Games() {
           <button>single</button>
         </div>
       </div>
+      <main>
+        {
+          loading ?
+          <p>loading...</p>
+          :
+          games.map((game) => {
+            return (
+              <div className={styles.game} key={game.id}>
+                <img src={game.background_image} alt="" />
+                <span>{game.name}</span>
+              </div>
+            );
+          })
+        }
+      </main>
 
     </div>
   );
