376758dce3d5995305258070601dc4eb9199fae1
muchubatactics <byooxmckeith@gmail.com>
2024-07-18 13:01:32 +0300

    Add: cart transitions

--- src/components/shopHeader.jsx
+++ src/components/shopHeader.jsx
@@ -3,15 +3,22 @@ import { Link } from "react-router-dom";
 import Searchbar from "./searchbar";
 
 import '../styles/shopheader.css';
+import { useState } from "react";
 
 export default function Shopheader() {
+  const [showDialog, setShowDiaog] = useState(false);
+
   return (
     <header className="shopheader">
       <Link to='/' className="logolink"><Logo /></Link>
       <Searchbar />
-      <div className="cart">
-      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M240-80q-33 0-56.5-23.5T160-160v-480q0-33 23.5-56.5T240-720h80q0-66 47-113t113-47q66 0 113 47t47 113h80q33 0 56.5 23.5T800-640v480q0 33-23.5 56.5T720-80H240Zm0-80h480v-480h-80v80q0 17-11.5 28.5T600-520q-17 0-28.5-11.5T560-560v-80H400v80q0 17-11.5 28.5T360-520q-17 0-28.5-11.5T320-560v-80h-80v480Zm160-560h160q0-33-23.5-56.5T480-800q-33 0-56.5 23.5T400-720ZM240-160v-480 480Z"/></svg>
+      <div className="cart" onClick={()=>{setShowDiaog(true)}}>
+        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed">
+          <path d="M240-80q-33 0-56.5-23.5T160-160v-480q0-33 23.5-56.5T240-720h80q0-66 47-113t113-47q66 0 113 47t47 113h80q33 0 56.5 23.5T800-640v480q0 33-23.5 56.5T720-80H240Zm0-80h480v-480h-80v80q0 17-11.5 28.5T600-520q-17 0-28.5-11.5T560-560v-80H400v80q0 17-11.5 28.5T360-520q-17 0-28.5-11.5T320-560v-80h-80v480Zm160-560h160q0-33-23.5-56.5T480-800q-33 0-56.5 23.5T400-720ZM240-160v-480 480Z"/>
+        </svg>
       </div>
+      <div className={showDialog ? "dialoghelp on" : "dialoghelp off"} onClick={() => {setShowDiaog(false)}}></div>
+      <div className={showDialog ? "cart-itself on": "cart-itself"}></div>
     </header>
   ); 
 }
