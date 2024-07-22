06ad1a6de647f56944d994d9e93778dd9737e146
muchubatactics <byooxmckeith@gmail.com>
2024-07-22 16:51:44 +0300

    Update: loading component

--- src/components/loading.jsx
+++ src/components/loading.jsx
@@ -3,7 +3,9 @@ import styles from '../styles/loading.module.css';
 export default function Loading() {
   return (
     <div className={styles.container}>
-      <div className={styles.loader}></div>
+      <svg viewBox="25 25 50 50">
+        <circle className={styles.circle} cx="50" cy="50" r="20"></circle>
+      </svg>
     </div>
   );
 }
