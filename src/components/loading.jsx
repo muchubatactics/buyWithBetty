9a305c1f8c402fff56b5ec52bed314a4bf490a92
muchubatactics <byooxmckeith@gmail.com>
2024-07-22 15:18:15 +0300

    Create: custom loading component

---
+++ src/components/loading.jsx
@@ -0,0 +1,9 @@
+import styles from '../styles/loading.module.css';
+
+export default function Loading() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.loader}></div>
+    </div>
+  );
+}
