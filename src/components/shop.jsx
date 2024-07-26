909af2c6f2f283d0f0ced891f9c1263ee15f431e
muchubatactics <byooxmckeith@gmail.com>
2024-07-26 10:46:33 +0300

    Update: mobile responsiveness for game page

--- src/components/shop.jsx
+++ src/components/shop.jsx
@@ -4,7 +4,7 @@ import { Outlet } from "react-router-dom";
 
 export default function Shop() {
   return (
-    <div style={{position: 'relative'}}>
+    <div style={{position: 'relative', height: '100%', boxSizing: 'border-box', display: 'flex', flexDirection: 'column'}}>
       <Shopheader />
       <PageTransitionWrapper>
         <Outlet />
